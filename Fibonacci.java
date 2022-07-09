d97035e96ef78997e60a85fb8172cb3d9febac50
RitikaRazdan <108994010+RitikaRazdan@users.noreply.github.com>
2022-07-09 23:11:46 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,30 @@
+package w1.d1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstNum=0;
+		int secNum = 1;
+		int sum;
+		
+		System.out.println(firstNum);
+		System.out.println(secNum);
+		
+		for(int i=1;i<11;i++)
+		{
+			sum = firstNum + secNum;
+			firstNum = secNum;
+			secNum = sum;
+			System.out.println(sum);
+		}
+
+	}
+
+}
+
+
+
+
+
+
